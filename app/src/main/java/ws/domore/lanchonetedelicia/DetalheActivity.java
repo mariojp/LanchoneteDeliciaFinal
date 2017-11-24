419732ee1eb26ff9ce529c7f7e5c2a8b318f490d
Mario Jorge Pereira <mariojp@gmail.com>
2017-11-23 21:22:44 -0300

    Initial commit

---
+++ app/src/main/java/ws/domore/lanchonetedelicia/DetalheActivity.java
@@ -0,0 +1,12 @@
+package ws.domore.lanchonetedelicia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
